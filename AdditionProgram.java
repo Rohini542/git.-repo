ced3b469cfaa78620c326ec03d547fcaa5d646c5
BadrapuAkshaya <akshaya14705@gmail.com>
2025-09-23 10:30:17 +0530

    break command is added

---
+++ AdditionProgram.java
@@ -0,0 +1,14 @@
+public class AdditionProgram {
+
+    public static void main(String[] args) {
+        // Declare and initialize two integer variables
+        int number1 = 15;
+        int number2 = 25;
+
+        // Calculate the sum
+        int sum = number1 + number2;
+
+        // Print the result to the console
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
